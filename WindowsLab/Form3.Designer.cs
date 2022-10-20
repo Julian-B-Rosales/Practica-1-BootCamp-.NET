1f1a170c2a6e6a3e2955b8ffc77991867a590f4f
julianRosales <rjulianbenjamin@gmail.com>
2022-10-20 14:48:25 -0300

    Ejercicio 3

---
+++ WindowsLab/Form3.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace WindowsLab
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.Location = new System.Drawing.Point(344, 310);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(75, 23);
+            this.btnMultiplicar.TabIndex = 0;
+            this.btnMultiplicar.Text = "Calcular";
+            this.btnMultiplicar.UseVisualStyleBackColor = true;
+            this.btnMultiplicar.Click += new System.EventHandler(this.btnMultiplicar_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(12, 196);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(382, 16);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Text = "Ingrese el numero del que desea obtener la tabla de multiplicar";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(479, 190);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(100, 22);
+            this.txtNumero.TabIndex = 2;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btnMultiplicar);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMultiplicar;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.TextBox txtNumero;
+    }
+}
---
+++ WindowsLab/Form3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsLab
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            int n = Convert.ToInt32(txtNumero.Text);
+            int aux;
+
+            string tabla = "";
+
+            for (int i = 0; i <= 12; i++)
+            {
+                aux = n * i;
+                tabla += Convert.ToString(n) + " * " + Convert.ToString(i) + " = " + Convert.ToString(aux) + "\n";
+            }
+
+            
+            MessageBox.Show("Los resultados de la tabla son: \n" + tabla);
+
+        }
+    }
+}
