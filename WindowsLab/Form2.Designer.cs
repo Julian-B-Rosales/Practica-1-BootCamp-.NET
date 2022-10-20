de218a5ab5c0db3acd2e5d4d699766917f3c8391
julianRosales <rjulianbenjamin@gmail.com>
2022-10-20 14:25:45 -0300

    Ejercicio 2

---
+++ WindowsLab/Form2.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace WindowsLab
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(318, 210);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(75, 23);
+            this.btnIngresar.TabIndex = 0;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(188, 102);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(107, 16);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Text = "Ingrese su edad:";
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(380, 102);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(166, 22);
+            this.txtEdad.TabIndex = 2;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btnIngresar);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.TextBox txtEdad;
+    }
+}
---
+++ WindowsLab/Form2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsLab
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            int edad = Convert.ToInt32(txtEdad.Text);
+
+            if (edad < 18 && edad > 0)
+            {
+                MessageBox.Show("Usted es menor de edad");
+            }
+            else if (edad >= 18 && edad < 60 )
+            {
+                MessageBox.Show("Usted es mayor de edad");
+            }
+            else if (edad >= 60)
+            {
+                MessageBox.Show("Usted en un adulto mayor");
+            }
+            else
+            {
+                MessageBox.Show("ERROR! La edad ingresada es incorrecta");
+            }
+        }
+    }
+}
