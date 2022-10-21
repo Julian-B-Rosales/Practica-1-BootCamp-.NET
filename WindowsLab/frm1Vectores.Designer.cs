51f2c5a5b360694e95556b83cf21be01237dc994
julianRosales <rjulianbenjamin@gmail.com>
2022-10-21 14:12:01 -0300

    Ejercicio 1 Loops y Vectores

---
+++ WindowsLab/frm1Vectores.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace WindowsLab
+{
+    partial class frm1Vectores
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(332, 214);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Calcular notas";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // frm1Vectores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCalcular);
+            this.Name = "frm1Vectores";
+            this.Text = "frm1Vectores";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
---
+++ WindowsLab/frm1Vectores.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsLab
+{
+    public partial class frm1Vectores : Form
+    {
+        public frm1Vectores()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int[] notas0 = new int[] {4,7,1,2};
+            int[] notas1 = new int[] {1,2,5,5};
+            int[] notas2 = new int[] {8,7,7,10};
+
+            string[] alumnos = new string[] { "juan", "jorge", "cesar" };
+
+            MessageBox.Show(" Las notas de " + alumnos[0] + " son " + String.Join("; ", notas0)
+                 + "\n Su nota mas alta fue " + mayorNota(notas0) + " y su promedio es " + promedio(notas0));
+            
+            MessageBox.Show(" Las notas de " + alumnos[1] + " son " + String.Join("; ", notas1)
+                 + "\n Su nota mas alta fue " + mayorNota(notas1) + " y su promedio es " + promedio(notas1));
+
+            MessageBox.Show(" Las notas de " + alumnos[2] + " son " + String.Join("; ", notas2)
+                 + "\n Su nota mas alta fue " + mayorNota(notas2) + " y su promedio es " + promedio(notas2));
+
+
+
+        }
+
+        private string mayorNota(int[] notas)
+        {
+            return notas.Max().ToString();
+        }
+
+        private double promedio(int[] notas)
+        {
+            double prom = 0;
+            for (int i = 0; i < notas.Length; i++)
+            {
+                prom += notas[i];
+            }
+
+            prom = prom/4;
+
+            if (prom < 4)
+            {
+                MessageBox.Show("Debe ir a recuperatorio");
+            }
+            else if (prom >= 4 && prom < 7)
+            {
+                MessageBox.Show("Bien.");
+            }
+            else if (prom >= 7)
+            {
+                MessageBox.Show("Muy Bien.");
+            }
+            return prom;
+        }
+    }
+}
